2f82ae5c5aff4702aa6e85be5d7c02013aaa7f6a
R-Enomoto <254188903+R-Enomoto-source@users.noreply.github.com>
2026-02-24 14:15:02 +0900

    ﻿Auto commit: 2026-02-24 14:14:57
    螟画峩蜀・ｮｹ:
    
    隧ｳ邏ｰ:
    On branch main Your branch is up to date with 'origin/main'.  Untracked files:   (use "git add <file>..." to include in what will be committed) 	java_blackbook/exercise/Chapter4/StaticInitializerDemo.java  nothing added to commit but untracked files present (use "git add" to track)

---
+++ java_blackbook/exercise/Chapter4/StaticInitializerDemo.java
@@ -0,0 +1,28 @@
+/**
+ * static初期化子の動作確認用サンプル（学習用に作成）
+ * クラスが初めて使われるときに static ブロックが1度だけ実行されることを示す。
+ */
+public class StaticInitializerDemo {
+    static int value;
+
+    static {
+        value = 10;
+        System.out.println("static初期化子: value = " + value);
+    }
+
+    public StaticInitializerDemo() {
+        value = 100;
+        System.out.println("コンストラクタ: value = " + value);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("--- main開始 ---");
+        System.out.println("クラス参照時点の value = " + StaticInitializerDemo.value);
+        System.out.println("--- 1つ目のインスタンス生成 ---");
+        StaticInitializerDemo a = new StaticInitializerDemo();
+        System.out.println("--- 2つ目のインスタンス生成 ---");
+        StaticInitializerDemo b = new StaticInitializerDemo();
+        System.out.println("最終的な value = " + StaticInitializerDemo.value);
+        System.out.println("--- main終了 ---");
+    }
+}
